2ddc4bfefd098e737881ab3f544db35c917eb38b
marcus pedersen <marcus@mpedersen.me>
2024-08-12 10:53:35 +0200

    Fix boilercode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 export function App() {
   return <>This is the App Component!</>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components";
 
-import "./css/index.css";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
