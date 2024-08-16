2acc3a0fe46004ca6a12ff845078a43fac77b77d
marcus pedersen <marcus@mpedersen.me>
2024-08-16 15:16:33 +0200

    Add a transition spinner for favourites

--- src/pages/FavouriteCocktailsPage.tsx
+++ src/pages/FavouriteCocktailsPage.tsx
@@ -1,9 +1,24 @@
 import { useOutletContext } from "react-router-dom";
 import SmallCocktailCard from "../components/ui/smallCocktailCard";
 import { ICocktailContext } from "../interfaces/interfaces";
+import { useEffect, useState } from "react";
+
+import "../css/FavouritePage.css";
+
+import { LoadingSpinner } from "../components/ui/Loading";
 
 export default function FavouriteCocktailsPage() {
   const { favourites } = useOutletContext<ICocktailContext>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 300);
+  });
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <>
       <h1>Favourites</h1>
--- src/pages/LandingPageRandomCocktail.tsx
+++ src/pages/LandingPageRandomCocktail.tsx
@@ -1,8 +1,8 @@
 import { ReactElement } from "react";
 import { useRandomCocktail } from "../hooks/useRandomCocktail";
 
-import CocktailCard from "../components/CocktailCard";
 import { NewRandomCocktailButton, SeeMoreButton } from "../components/ui/Buttons";
+import CocktailCard from "../components/CocktailCard";
 import { LoadingSpinner } from "../components/ui/Loading";
 
 export default function LandingPageRandomCocktail(): ReactElement {
